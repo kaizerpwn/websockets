979cfbbf5fd5fce70c292ece7b8f8f971621f924
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2023-08-04 17:53:05 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,26 @@
-import * as dotenv from "dotenv";
-import { Server as WebSocketServer } from "ws";
+const dotenv = require("dotenv");
+const { Server: WebSocketServer } = require("ws");
 
 dotenv.config();
 const webSocketServer = new WebSocketServer({
   port: Number(process.env.SERVER_PORT),
 });
 
+function extractIPv4(address) {
+  if (address.includes("::ffff:")) {
+    return address.split("::ffff:")[1];
+  }
+  return address;
+}
+
 webSocketServer.on("listening", () => {
   console.log(`>> Websocket server running on port ${process.env.SERVER_PORT}`);
 });
 
 webSocketServer.on("connection", (ws) => {
-  console.log("New client connected!");
+  console.log(
+    `New client (${extractIPv4(ws._socket.remoteAddress)}) connected!`
+  );
   ws.on("close", () => console.log("Client has disconnected!"));
   ws.on("open", () =>
     console.log(
